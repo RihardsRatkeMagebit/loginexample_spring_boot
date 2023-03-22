c330b4ef66fe39e09811f51c764a4b8331ead36d
Rihards Ratke <rihards.ratke@magebit.com>
2023-03-22 13:19:27 +0200

    - Added screenshots

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -12,34 +12,40 @@ public class BasicController {
     // https://www.w3schools.com/tags/ref_httpmethods.asp
 
     // GET is used to resource from server
+    // GET http://localhost:8080/basic/hello
+    // View screenShots/PostMan/Controllers/basic_get.png
     @GetMapping("hello")
     public String getHelloWorld() {
         return "Hello world GET";
     }
 
     // POST is used to send data to server for all actions
+    // POST http://localhost:8080/basic/hello
+    // View screenShots/PostMan/Controllers/basic_post.png
     @PostMapping("hello")
     public String postHelloWorld(){
         return "Hello world POST";
     }
 
     // PUT is used to send data to a server to create/update a resource
+    // PUT http://localhost:8080/basic/hello
     @PutMapping("hello")
     public String putHelloWorld(){
         return "Hello world PUT";
     }
 
     // DELETE is used to send data to server to delete a resource
+    // DELETE http://localhost:8080/basic/hello
+    // View screenShots/PostMan/Controllers/basic_delete.png
     @DeleteMapping("hello")
     public String deleteHelloWorld(){
         return "Hello world DELETE";
     }
 
     // PATCH is used to send data to server to update a resource
+    // PATCH http://localhost:8080/basic/hello
     @PatchMapping("hello")
     public String patchHelloWorld(){
         return "Hello world PATCH";
     }
-
-
 }
