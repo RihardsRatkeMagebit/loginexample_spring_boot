d39ba4827043a7902bc25d3d87fc74965a27979f
Rihards Ratke <rihards.ratke@magebit.com>
2023-03-24 15:43:17 +0200

    Removed security node

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,17 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
 
-        httpSecurity.formLogin();
-        httpSecurity.httpBasic();
-
-        return httpSecurity.build();
-    }
 }
