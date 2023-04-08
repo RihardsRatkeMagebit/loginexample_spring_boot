224cfed93e8ed1528b4357353f296fedbed181a9
Rihards Ratke <rihards.ratke@magebit.com>
2023-04-08 22:12:44 +0300

    Added formated error handling

--- src/main/java/com/example/demo/controller/advice/RestErrorHandler.java
+++ src/main/java/com/example/demo/controller/advice/RestErrorHandler.java
@@ -8,21 +8,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.EscapedErrors;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.nio.file.AccessDeniedException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-@RestController
-@Slf4j
 public class RestErrorHandler extends ResponseEntityExceptionHandler {
     Logger logger = LoggerFactory.getLogger(RestErrorHandler.class);
 
@@ -48,5 +55,25 @@ public class RestErrorHandler extends ResponseEntityExceptionHandler {
         logger.error(exception.getMessage());
 
         return GenericResponse.generateResponse("Token is invalid or experienced, re-auth required", HttpStatus.OK, null);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        Map<String, List<String>> body = new HashMap<>();
+
+        List<String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.example.demo.dto;
 
-import jakarta.persistence.Enumerated;
+import com.example.demo.anotation.ValueOfEnum;
+import com.example.demo.model.Role;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
@@ -12,14 +14,20 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
-    @Pattern(regexp = "[a-z]{2,}", message = "username must be at-least 2 chars long and all lowercase")
+
+    @NotBlank(message = "Username is a required field")
+    @Pattern(regexp = "[a-z]{2,}", message = "Username must be at-least 2 chars long and all lowercase")
     private String username;
 
+
+    @NotBlank(message = "password is a required field")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
             message = "Password must contain at-least 8 characters and 1 lower case letter and 1 upper case latter and 1 symbol"
     )
     private String password;
 
+    @NotBlank(message = "Role is a required field")
+    @ValueOfEnum(enumClass = Role.class, message = "Invalid role")
     private String role;
 }
